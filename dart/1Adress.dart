084f5115f677ac82edd656543d82e7fb7e7acb63
Vanshdarji <134920483+Vanshdarji@users.noreply.github.com>
2023-12-07 18:59:19 +0530

    Add files via upload

---
+++ dart/1Adress.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  String name = "vansh darji";
+  String birthDate = "24/01/2002";
+  int age = calculateAge("21"); 
+  String address = "sagwara";
+
+  print("Name: $name");
+  print("Birth Date: $birthDate");
+  print("Age: $age");
+  print("Address: $address");
+}
+
+int calculateAge(String birthDate) {
+  return 23;
+}
